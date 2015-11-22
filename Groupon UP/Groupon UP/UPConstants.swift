b225ba4a49683aef2545b3d71f6780999fb1597c
Robert Xue <rxue@groupon.com>
2015-11-22 11:08:45 -0600

    Add profile screen

--- Groupon UP/Groupon UP/UPConstants.swift
+++ Groupon UP/Groupon UP/UPConstants.swift
@@ -10,3 +10,8 @@ import UIKit
 
 let UPBackgroundColor = UIColor.whiteColor()
 let UPTintColor = UIColor(rgba: "#82b548")
+
+let UPSpanSize = 15
+let UPPrimaryTextColor = UIColor(rgba: "#333333")
+let UPSecondaryTextColor = UIColor(rgba: "#888888")
+let UPContentFont = UIFont(name: "Avenir", size: 17)!
